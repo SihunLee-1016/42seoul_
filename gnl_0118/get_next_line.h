28724cb1b653299b970e64ac99f153a406f9a227
Sihun Lee <silee@c6r6s1.42seoul.kr>
2022-01-19 16:36:10 +0900

    gnl_0119

--- gnl_0118/get_next_line.h
+++ gnl_0118/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: silee <silee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 16:38:59 by silee             #+#    #+#             */
-/*   Updated: 2022/01/17 20:42:55 by silee            ###   ########.fr       */
+/*   Updated: 2022/01/19 16:35:40 by silee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 # include <stdlib.h>
 # include <stdio.h>
 char	*get_next_line(int fd);
-char	*gnl_next(char **next, char *tmp);
-void	ft_strjoin(char *s1, char *s2, int *line_len);
-char	*ft_strdup(char *src,int *line_len);
-char	*ft_strcat(char *res, char const *buffer);
-int	ft_strlen(char *str);
+char    *gnl_next(char *next, char **tmp, int *signal);
+char	*ft_substr(char *s, int start, int len, int *signal);
+char	*ft_strjoin(char *s1, char *s2, int *signal);
+char	*ft_strcat(char *res, char *buffer);
+int     ft_strlen(char *str);
+
 
 #endif
