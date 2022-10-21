c0fe6ab37777e1d12a696b3f08e92ef8a29efd65
Sihun Lee <silee@c8r2s8.42seoul.kr>
2022-10-21 18:02:17 +0900

    1021_1802

--- 4_circle/cpp/cpp00/ex01/phone_book.cpp
+++ 4_circle/cpp/cpp00/ex01/phone_book.cpp
@@ -1,119 +1,176 @@
 #include <iostream>
+#include <iomanip>
 #include <string.h>
-//class는 2개.
-// 연락처의 배열. 8개까지 저장 가가능능
-// 8개까지 저장 된 뒤 9번째가 입력된다면 가장 최근 저장된 8번째를 삭제하고 입력된 값을 저장
-
 class contact
 {
-    private:
-        std::string first;
-        std::string last;
-        std::string nickname;
-        std::string phone_num;
-        std::string secret;
-    public :
-        void    data_Add(void);
-        void    data_Search(void);
-        void    data_Exit(void);
-        void    data_print(void);
+	private:
+		std::string idx;
+		std::string first;
+		std::string last;
+		std::string nickname;
+		std::string phone_num;
+		std::string secret;
+	public :
+		void    data_Add(void);
+		void    data_Search(int	idx);
+		void	data_print(int i);
 };
 
 class phone_book
 {
-    private :
-        contact contacts[8];
-        int     idx;
-    public :
-        phone_book(void) {
-            contacts {};
-            this->idx = 0;
-        }
-        void    cnt_Add(void);
-        void    cnt_Search(void);
-        void    cnt_Exit(void);
+	private :
+		contact contacts[8];
+		int     idx;
+	public :
+		phone_book(void) {
+			this->idx = 0;
+		}
+		void    cnt_Add(void);
+		void    cnt_Search(void);
+		void	print_all(void);
 };
 
 void    phone_book::cnt_Add(void)
 {
-    this->contacts[idx % 8].data_Add();
-    this->idx++;
+	if (this->idx <= 7)
+	{
+		this->contacts[this->idx % 8].data_Add();
+		this->idx++;
+	}
+	else if (this->idx > 7)
+	{
+		this->contacts [7].data_Add();
+		this->idx++;
+	}
+}
+
+void    contact::data_Add(void)
+{
+	std::cout << "First name : ";
+	std::cin >> this->first;
+
+	std::cout << "Last name : ";
+	std::cin >> this->last;
+	
+	std::cout << "Nick name : ";
+	std::cin >> this->nickname;
+	
+	std::cout << "Phone number : ";
+	std::cin >> this->phone_num;
+
+	std::cout << "Secret : ";
+	std::cin >> this->secret;
 }
 
 void    phone_book::cnt_Search(void)
 {
+	int s_idx;
 
+	if (this->idx == 0)
+	{
+		std::cout << "Phone book empty" << std::endl;
+		return ;
+	}
+	std::cout << "Which one u wanna search? : ";
+	std::cin >> s_idx;
+	std::cout << std::endl;
+	s_idx -= 1;
+	if (s_idx > 7 || s_idx < 0)
+	{
+		std::cout << "Wrong index input" << std::endl;
+		return ;
+	}
+	if (s_idx >= this->idx)
+	{
+		std::cout << "Wrong index input" << std::endl;
+		return ;
+	}
+	this->contacts[s_idx].data_Search(s_idx);
 }
 
-void    phone_book::cnt_Exit(void)
+void	contact::data_print(int	i)
 {
+	int k = 0;
 
+	std::cout << std::right;
+	std::cout << "     Index	|";
+	std::cout << std::setw(10) << i + 1 << std::endl;
+	std::cout << "First name	|";
+	std::cout << std::setw(10);
+	
+	while (k <= 9 && this->first[k])
+	{
+		std::cout << this->first[k];
+		k++;
+	}
+	if (k == 10)
+		std::cout << ".";
+	std::cout << std::endl;
+	k = 0;
+	std::cout << " Last name	|";
+	std::cout << std::setw(10);
+	while (i <= 9 && this->last[k])
+	{
+		std::cout << this->last[k];
+		k++;
+	}
+	if (k == 10)
+		std::cout << ".";
+	std::cout << std::endl;
+	k = 0;
+	std::cout << " Nick name	|";
+	std::cout << std::setw(10);
+	while (k <= 9 && this->nickname[k])
+	{
+		std::cout << this->nickname[k];
+		k++;
+	}
+	if (k == 10)
+		std::cout << ".";
+	std::cout << std::endl;
 }
-void    contact::data_Add(void)
+
+void    contact::data_Search(int idx)
 {
-    std::cout << "first name" << std::endl;
-    std::cin >> this->first;
-    std::cout << "last name" << std::endl;
-    std::cin >> this->last;
-    std::cout << "nick name" << std::endl;
-    std::cin >> this->nickname;
-    std::cout << "phone number" << std::endl;
-    std::cin >> this->phone_num;
-    std::cout << "secret" << std::endl;
-    std::cin >> this->secret;
+	this->data_print(idx);
 }
 
-void    contact::data_Search(void)
+void	phone_book::print_all(void)
 {
+	int	i = 0;
 
-}
-
-void    contact::data_Exit(void)
-{
-
-}
-
-void    contact::data_print(void)
-{
-    std::cout << "first : " << this->first << std::endl;
+	while (i < this->idx && i < 8)
+	{
+		this->contacts[i].data_print(i);
+		std::cout << std::endl;
+		i++;
+	}
 }
 
 int main(void)
 {
-    std::string cmd;
-    phone_book  data;
-    int     idx = 0;
-    int     i = 0;
+	std::string cmd;
+	phone_book  data;
+	int     idx = 0;
+	int     i = 0;
 
-    while (idx != 3)
-    {
-        std::cout << "cmd (ADD, SEARCH, EXIT)" << std::endl;
-        std::cin >> cmd;
-        if (cmd == "ADD")
-        {
-            data.cnt_Add();
-            idx++;
-        }
-        else if (cmd == "SEARCH")
-        {
-
-        }
-        else if (cmd == "EXIT")
-        {
-            
-        }
-        else
-        {
-            std::cout << "cmd not found" << std::endl;
-        }
-    }
-
-    while (i < idx)
-    {
-        data[idx].cnt_print();
-        i++;
-    }
-
-    return (0);
-
+	while (!std::cin.eof())
+	{
+		std::cout << "Command (add, search, exit) : ";
+		std::cin >> cmd;
+		if (cmd == "add")
+			data.cnt_Add();
+		else if (cmd == "search")
+		{
+			data.print_all();
+			data.cnt_Search();
+		}
+		else if (cmd == "exit")
+		{
+			exit(0);
+		}            
+		else
+			std::cout << "cmd not found" << std::endl;
+	}
+	return (0);
 }
