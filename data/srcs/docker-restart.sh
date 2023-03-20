c05a146edb618d80d28751ae0bedc2ff394372a1
SihunLee-1016 <82656136+SihunLee-1016@users.noreply.github.com>
2023-03-20 22:02:38 +0900

    Delete data directory

--- data/srcs/docker-restart.sh
+++
@@ -1,4 +0,0 @@
-sudo docker-compose -f docker-compose.yml down
-sudo docker volume rm $(sudo docker volume ls -q)
-sudo docker-compose -f docker-compose.yml up --build
-sudo docker exec -it mariadb sh
